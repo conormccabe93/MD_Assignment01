4d31400904c4c8a6be9517c061d9c9ec5a6b38e5
Conor McCabe <conor.mc.cabe@hotmail.com>
2022-11-14 18:31:15 +0000

    Create Data Function added + Sample data created

--- app/src/main/java/com/example/md_assigment01_3009601/MainActivity.kt
+++ app/src/main/java/com/example/md_assigment01_3009601/MainActivity.kt
@@ -27,13 +27,30 @@ class MainActivity : AppCompatActivity() {
         mapRecyclerView.adapter = MapAdapter(this, test1)
     }
 
+    // Create Sample data to test display
     private fun testData(): List<UserCreatedMap>{
         return listOf(
-            UserCreatedMap("Test Map 1",
-            listOf(
-                Place("Site 1", "Site 1 Test description for random Co-crdinates", 1.212, -83.443)
-            )
-            )
+            UserCreatedMap("Test Map 1 - Random",
+                listOf(
+                    Place("Site 1", "Site 1 Test description for random Co-ordinates", 1.212, -83.443),
+                    Place("Site 2", "Site 2 Test Description for Random co-ordinates",1.999,-82.111 )
+                    )
+                ),
+            UserCreatedMap("Test Map 2 - Paris",
+                listOf(
+                    Place("Eiffel Tower", "The Eiffel Tower", 48.858,2.294),
+                    Place("The Louvre", "The Louvre Museum",48.861, 2.332 ),
+                    Place("Arch The Triumph", "The Arch The Triumph", 48.878, 2.296)
+                    )
+                ),
+            UserCreatedMap("Test Map 3 - Dublin Ireland",
+                listOf(
+                    Place("Dublin Castle", "The Dublin Castle",53.343, -6.267),
+                    Place("Ha'penny Bridge", "The Ha'penny Birdge",53.346, -6.263),
+                    Place("O'Connell Bridge", " The O'Connell Brdige",53.347, -6.259),
+                    Place("Trinity College", "The Trinity College", 53.343, -6.254)
+                    )
+                )
         )
     }
 }
