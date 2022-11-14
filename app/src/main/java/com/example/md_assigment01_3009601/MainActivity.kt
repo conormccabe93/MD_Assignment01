1ee67507ac7d2f8266300808bb7a5e73a6e3bcd0
Conor McCabe <conor.mc.cabe@hotmail.com>
2022-11-14 18:07:31 +0000

    Test Data function added + Test 1 map display

--- app/src/main/java/com/example/md_assigment01_3009601/MainActivity.kt
+++ app/src/main/java/com/example/md_assigment01_3009601/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.md_assigment01_3009601.models.Place
+import com.example.md_assigment01_3009601.models.UserCreatedMap
 
 class MainActivity : AppCompatActivity() {
     // define recycler view
@@ -13,12 +15,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Call testData function for test
+        val test1 = testData()
+
         // Initialize recyclerview variable
         mapRecyclerView = findViewById(R.id.mapRecyclerView)
         //Create layout Manager for list of User maps
         mapRecyclerView.layoutManager = LinearLayoutManager(this)
 
         // Create adapter for displaying data from user
-        mapRecyclerView.adapter = MapAdapter(this, emptyList())
+        mapRecyclerView.adapter = MapAdapter(this, test1)
+    }
+
+    private fun testData(): List<UserCreatedMap>{
+        return listOf(
+            UserCreatedMap("Test Map 1",
+            listOf(
+                Place("Site 1", "Site 1 Test description for random Co-crdinates", 1.212, -83.443)
+            )
+            )
+        )
     }
 }
