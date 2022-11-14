2575ab78ca1aeef5d7db914c842c6df9d2a1792b
Conor McCabe <conor.mc.cabe@hotmail.com>
2022-11-14 17:35:03 +0000

    Comments added for data classes

--- app/src/main/java/com/example/md_assigment01_3009601/models/Place.kt
+++ app/src/main/java/com/example/md_assigment01_3009601/models/Place.kt
@@ -1,3 +1,5 @@
 package com.example.md_assigment01_3009601.models
 
+// Data class for each Place a user selects from the google maps API
+// Will take the a name and description along with the co-ordinates given by the Google Maps via its Long/Lat
 data class Place(val title: String, val description: String, val latitude: Double, val longitude: Double)
--- app/src/main/java/com/example/md_assigment01_3009601/models/UserCreatedMap.kt
+++ app/src/main/java/com/example/md_assigment01_3009601/models/UserCreatedMap.kt
@@ -1,3 +1,5 @@
 package com.example.md_assigment01_3009601.models
 
+// Data class for each new map a User creates
+// Includes Name of map and each place they choose
 data class UserCreatedMap(val title: String, val places: List<Place>)
