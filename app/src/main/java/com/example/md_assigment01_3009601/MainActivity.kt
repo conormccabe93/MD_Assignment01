937cb55c1cbede64c874754924de5d00aed596cf
Conor McCabe <conor.mc.cabe@hotmail.com>
2022-11-14 16:31:15 +0000

    Recycler view added for User maps

--- app/src/main/java/com/example/md_assigment01_3009601/MainActivity.kt
+++ app/src/main/java/com/example/md_assigment01_3009601/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.md_assigment01_3009601
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    // define recycler view
+    private lateinit var mapRecyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize recyclerview variable
+        mapRecyclerView = findViewById(R.id.mapRecyclerView)
+        //Create layout Manager for list of User maps
+        mapRecyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
